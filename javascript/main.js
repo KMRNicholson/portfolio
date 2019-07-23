2eb28d2a9e449e005bc81d19a74bbdf91a51a2e6
kmrnicholson <kohdy.nicholson@gmail.com>
2019-07-23 13:33:25 +0200

    First commit

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+document.querySelector('#navbar').innerHTML = `
+<div class="container">
+    <ul>
+        <li><a href="./aboutme.html"><span>About Me</span></a></li>
+        <li><a href="./portfolio.html"><span>Portfolio</span></a></li>
+        <li><a href="./blog.html"><span>Blog</span></a></li>
+        <li><a href="./contact.html"><span>Contact</span></a></li>
+    </ul>
+</div>`
+
+document.querySelector('#main-header').innerHTML = `<section id="showcase"></section>`
