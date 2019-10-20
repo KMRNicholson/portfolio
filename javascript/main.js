dfcba34b154ed410a5d6d5e10ed92f8538932427
kmrnicholson <kohdy.nicholson@gmail.com>
2019-10-20 15:54:56 +0200

    header opacity and height transitions

--- javascript/main.js
+++ javascript/main.js
@@ -9,3 +9,16 @@ document.querySelector('#navbar').innerHTML = `
 </div>`
 
 document.querySelector('#main-header').innerHTML = `<section id="showcase"></section>`
+
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("showcase").style.opacity = 0.2;
+    document.getElementById("showcase").style.minHeight = "200px"
+  } else {
+    document.getElementById("showcase").style.opacity = 1;
+    document.getElementById("showcase").style.minHeight = "600px"
+  }
+}
